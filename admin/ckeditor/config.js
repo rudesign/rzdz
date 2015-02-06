aeac511b6b144280ea31989a17cc4b29aa462ecb
Alex <220619@gmail.com>
2015-02-06 11:02:22 +0300

    ckeditor config modified (turquoise color added)

--- admin/ckeditor/config.js
+++ admin/ckeditor/config.js
@@ -22,7 +22,7 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
 		{ name: 'paragraph',   groups: [ 'list', 'blocks', 'align', 'bidi' ] },
 		{ name: 'styles' },
-		{ name: 'colors' },
+		{ name: 'colors' }
 	];
 
     config.extraPlugins = 'justify';
@@ -34,12 +34,16 @@ CKEDITOR.editorConfig = function( config ) {
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;h4;h5;pre';
 
+	config.defaultLanguage = 'ru';
+
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
 
     config.defaultLanguage = 'ru';
 
     config.autoGrow_maxHeight = '100%';
+
+	config.colorButton_colors = '000,800000,8B4513,2F4F4F,008c95,000080,4B0082,696969,B22222,A52A2A,DAA520,006400,40E0D0,0000CD,800080,808080,F00,FF8C00,FFD700,008000,0FF,00F,EE82EE,A9A9A9,FFA07A,FFA500,FFFF00,00FF00,AFEEEE,ADD8E6,DDA0DD,D3D3D3,FFF0F5,FAEBD7,FFFFE0,F0FFF0,F0FFFF,F0F8FF,E6E6FA,FFF';
 
     config.allowedContent = true;
 };
