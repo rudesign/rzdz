abab9f584d4d57b84767feb85007fec6d2627786
Alex <220619@gmail.com>
2015-04-07 10:38:52 +0300

    contraindications page refactored

---
+++ cure_alert1.php
@@ -0,0 +1,35 @@
+<style>
+    a{
+        color: #008c95;
+    }
+    .u{
+        text-transform: uppercase;
+    }
+    .content{
+        width: 600px;
+        height: 400px;
+        color: #636363;
+        font-family: Tahoma,Arial,sans-serif;
+        font-size: 13px;
+        line-height: 1.3;
+    }
+    .centered{
+        text-align: center;
+    }
+</style>
+
+<div class="content">
+    <p class="centered u"><b>Внимание!</b></p>
+    <p>&nbsp;</p>
+    <p>
+        Санаторно-курортное лечение является эффективным и интенсивным методом воздействия и имеет <a href="/medicine/14" target="_blank"><b>противопоказания</b></a>.
+    </p>
+    <p>&nbsp;</p>
+    <p>
+        Выбирая санаторий обязательно посоветуйтесь с Вашим лечащим врачом.
+    </p>
+    <p>&nbsp;</p>
+    <p>
+        Полный список противопоказаний к санаторно- курортному лечению содержится <a href="/medicine/14" target="_blank"><b>здесь</b></a>.
+    </p>
+</div>
