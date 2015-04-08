44a5f6f4b615d42fd53cd056da00cf6035c0134b
Alex <220619@gmail.com>
2015-04-08 11:29:34 +0300

    medicine/contraindications fixed

--- cure_alert1.php
+++ cure_alert1.php
@@ -18,18 +18,52 @@
     }
 </style>
 
-<div class="content">
-    <p class="centered u"><b>Внимание!</b></p>
-    <p>&nbsp;</p>
-    <p>
-        Санаторно-курортное лечение является эффективным и интенсивным методом воздействия и имеет <a href="/medicine/14" target="_blank"><b>противопоказания</b></a>.
-    </p>
-    <p>&nbsp;</p>
-    <p>
-        Выбирая санаторий обязательно посоветуйтесь с Вашим лечащим врачом.
-    </p>
-    <p>&nbsp;</p>
-    <p>
-        Полный список противопоказаний к санаторно- курортному лечению содержится <a href="/medicine/14" target="_blank"><b>здесь</b></a>.
-    </p>
-</div>
+<?php
+
+try{
+    $config = $data = array();
+
+    if(!@include('admin/config_site.php')) throw new \Exception('Отсутствует файл конфигурации');
+
+    if(!$connect = @mysql_connect($config['dbhost'], $config['dblogin'], $config['dbpassword'])) throw new \Exception('Нет подключения к базе данных');
+    if(!@mysql_select_db($config['dbname'], $connect)) throw new \Exception('Не выбрана база данных');
+
+    $query = "SELECT name, description as description FROM zdor_cure WHERE cure_id=14";
+
+    if($queryResult = @mysql_query($query)) {
+        if($result = @mysql_fetch_array($queryResult)) {
+            $data['att_name'] = $result['name'];
+            $data['att_description'] = $result['description'];
+        }else throw new \Exception;
+    }else throw new \Exception;
+
+    $query = "SELECT cure_id, name FROM zdor_cure WHERE parent=14";
+
+    if($queryResult = @mysql_query($query)) {
+        while ($result = @mysql_fetch_array($queryResult)) {
+            $list[] = array('name' => $result['name'], 'link' => "/medicine/14/" . $result['cure_id']);
+        }
+        $data['att_list'] = $list;
+    }else throw new \Exception;
+
+    show($data);
+
+}catch (\Exception $e){
+    echo $e->getMessage() ? $e->getMessage() : 'Ошибка в строке '.$e->getLine();
+}
+
+function show($data){
+    if(!empty($data)) {
+        echo '
+        <div class="content">
+            <p class="centered u"><b>'.$data['att_name'].'</b></p>
+            <p>&nbsp;</p>
+            <p>'.$data['att_description'].'</p>
+            <p>&nbsp;</p>
+            <p class="centered"><a href="/medicine/1238" target="_blank"><b>Полный список противопоказаний</b></a></p>
+        </div>
+        </div>';
+    }else{
+        echo 'Нет данных';
+    }
+}
