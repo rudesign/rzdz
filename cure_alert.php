4a8880825b337a786bed2e42007218e1004dce27
nikuha <yoginika@yandex.ru>
2015-04-21 13:21:37 +0300

    nika

--- cure_alert.php
+++ cure_alert.php
@@ -1,62 +1,65 @@
+<?
+
+require 'admin/config.php';
+require 'admin/lang.php';
+require 'lib/func.php';
+
+session_name(SES_NAME);
+session_start();
+
+global $englang, $lprefix, $lang_phrases;
+$r_u = ereg_replace("^(\/)?", "", getenv('REQUEST_URI'));  
+$r_u = ereg_replace("(\/)?(\?.*)?$", "", $r_u);  
+$rus_url = preg_replace("/(^|\/)en\/?/", '', getenv('REQUEST_URI'));
+$eng_url = preg_match("~^/~", $rus_url) ?  "/en".$rus_url : "/en/".$rus_url;
+
+$lprefix = $englang ? '/en' : '';
+$lang_phrases = array();
+$nn = $englang ? "name$englang" : 'name';
+foreach($lang_settings as $k=>$v) $lang_phrases[$k] = $k=='price' ? str_replace("Р", "<span class=\"rub\">a</span>", $v[$nn]) : $v[$nn];
+
+dbconnect($config['dbhost'], $config['dbname'], $config['dblogin'], $config['dbpassword']);
+
+
+$q = "SELECT name$englang as name, description$englang as description FROM ".TABLE_CURE." WHERE cure_id=14";
+$sql1 = mysql_query($q) or Error(1, __FILE__, __LINE__);
+$info = @mysql_fetch_array($sql1);
+$name = $info['name'];
+$description = $info['description'];
+
+	
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+<meta content="text/html; charset=windows-1251" http-equiv=Content-Type>
 <style>
     a{
         color: #008c95;
     }
+    .u{
+        text-transform: uppercase;
+    }
     .content{
+        width: 600px;
+        height: 400px;
         color: #636363;
         font-family: Tahoma,Arial,sans-serif;
         font-size: 13px;
         line-height: 1.3;
     }
-</style>
-<?php
-
-try{
-    $config = $data = array();
-
-    if(!@include('admin/config_site.php')) throw new \Exception('Отсутствует файл конфигурации');
-
-    if(!$connect = @mysql_connect($config['dbhost'], $config['dblogin'], $config['dbpassword'])) throw new \Exception('Нет подключения к базе данных');
-    if(!@mysql_select_db($config['dbname'], $connect)) throw new \Exception('Не выбрана база данных');
-
-    $query = "SELECT name, description as description FROM zdor_cure WHERE cure_id=14";
-
-    if($queryResult = @mysql_query($query)) {
-        if($result = @mysql_fetch_array($queryResult)) {
-            $data['att_name'] = $result['name'];
-            $data['att_description'] = $result['description'];
-        }else throw new \Exception;
-    }else throw new \Exception;
-
-    $query = "SELECT cure_id, name FROM zdor_cure WHERE parent=14";
-
-    if($queryResult = @mysql_query($query)) {
-        while ($result = @mysql_fetch_array($queryResult)) {
-            $list[] = array('name' => $result['name'], 'link' => "/medicine/14/" . $result['cure_id']);
-        }
-        $data['att_list'] = $list;
-    }else throw new \Exception;
-
-    show($data);
-
-}catch (\Exception $e){
-    echo $e->getMessage() ? $e->getMessage() : 'Ошибка в строке '.$e->getLine();
-}
-
-function show($data){
-
-    if(!empty($data)) {
-        echo '
-        <div class="content">
-            <p><b>1'.$data['att_name'].'</b></p>
-            '.$data['att_description'].'
-            <p>';
-                foreach($data['att_list'] as $v){
-                    echo '<a href="'.$v['link'].'">'.$v['name'].'</a><br />';
-                }
-            echo '</p>
-        </div>';
-    }else{
-        echo 'Нет данных';
+    .centered{
+        text-align: center;
     }
-}
+</style>
+  </head>
+  <body>
+  <div class="content">
+            <p class="centered u"><b><?=$name?></b></p>
+            <p>&nbsp;</p>
+            <p><?=$description?></p>
+            <p>&nbsp;</p>
+            <p class="centered"><a href="/medicine/14" target="_blank"><b>Полный список противопоказаний</b></a></p>
+  </div>
+  </body>
+ </html>
